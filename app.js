5b4c447ba675a0b54b3e0ac8b02db7cde29f520c
Nicolas Neira <neiranicolas17@gmail.com>
2023-10-01 01:15:37 -0300

    add database conection, new flows and other fix

--- app.js
+++ app.js
@@ -4,95 +4,58 @@ const { createBot, createProvider, createFlow, addKeyword } = require('@bot-what
 
 const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
-const PostgreSQLAdapter = require('@bot-whatsapp/database/postgres')
-// const MockAdapter = require('@bot-whatsapp/database/mock')
+const CustomPostgresAdapter = require('./postgres.adapter')
 
-/**
- * Declaramos las conexiones de PostgreSQL
- */
-
-
-/**
- * Aqui declaramos los flujos hijos, los flujos se declaran de atras para adelante, es decir que si tienes un flujo de este tipo:
- *
- *          Menu Principal
- *           - SubMenu 1
- *             - Submenu 1.1
- *           - Submenu 2
- *             - Submenu 2.1
- *
- * Primero declaras los submenus 1.1 y 2.1, luego el 1 y 2 y al final el principal.
- */
 
 const palabrasClaveBienvenida = ['hola', 'buenas', 'volver']
 
-const bookList = [
-    {
-        "name": "Don Quijote de la Mancha",
-        "author": "Miguel de Cervantes"
-    },
-    {
-        "name": "Cien años de soledad",
-        "author": "Gabriel García Márquez"
-    },
-    {
-        "name": "1984",
-        "author": "George Orwell"
-    },
-    {
-        "name": "Matar a un ruiseñor",
-        "author": "Harper Lee"
-    },
-    {
-        "name": "Ulises",
-        "author": "James Joyce"
-    },
-    {
-        "name": "En busca del tiempo perdido",
-        "author": "Marcel Proust"
-    },
-    {
-        "name": "El gran Gatsby",
-        "author": "F. Scott Fitzgerald"
-    },
-    {
-        "name": "Orgullo y prejuicio",
-        "author": "Jane Austen"
-    },
-    {
-        "name": "El señor de los anillos",
-        "author": "J.R.R. Tolkien"
-    },
-    {
-        "name": "Crimen y castigo",
-        "author": "Fyodor Dostoevsky"
-    }
-]
+const dbAdapter = new CustomPostgresAdapter({
+    host: process.env.POSTGRES_DB_HOST,
+    user: process.env.POSTGRES_DB_USER,
+    database: process.env.POSTGRES_DB_NAME,
+    password: process.env.POSTGRES_DB_PASSWORD,
+    port: process.env.POSTGRES_DB_PORT
+});
 
 // !! No utilizar buttons ya que no funciona correctamente y no te devuelve ningun mensaje con el proveedor baileys 
 
-
-const flowSearch = addKeyword('stock')
-    .addAnswer(['Ingresa el titulo del libro que deseas buscar \n', 'o *volver* para regresar al menu principal'], { capture: true },
+const flowSearch = addKeyword('buscar')
+    .addAnswer(['Ingresa el titulo o autor del libro que deseas buscar \n', 'o *volver* para regresar al menu principal'], { capture: true },
         async (ctx, { flowDynamic }) => {
             console.log('Se ha iniciado la busqueda del titulo: ', ctx.body);
 
+            if (ctx.body !== 'volver' && ctx.body !== 'horario') {
+                const textSearch = ctx.body;
 
-            // Todo: Hacer que se pueda volver a realizar otra busqueda ( Sin tener que volver al menu principal )
-            if (ctx.body !== 'volver' || ctx.body !== 'horario') {
-                const titulo = ctx.body;
-                const resultados = bookList.filter(libro => new RegExp(titulo, 'i').test(libro.name));
-                const mensajes = resultados.map(libro => ({ body: `Titulo: ${libro.name}, Autor: ${libro.author}` }));
-                await flowDynamic(mensajes.length > 0
-                    ? [{
-                        body: `Si poseemos el libro: "${mensajes.map(mensaje => mensaje.body).join(', ')}" en nuestro inventario.
-                        \n Escribe "*volver*" para regresar al menu principal` }]
-                    : [{
-                        body: `Lo siento, no pudimos encontrar el libro que estás buscando en nuestro inventario.
-                        \n Escribe "*volver*" para regresar al menu principal` }]
-                );
+                // Consultamos la base de datos para buscar el libro por el título
+                try {
+                    const result = await dbAdapter.searchBookByTitle(textSearch);
+                    console.log('result.rows.lenght', result.length);
+
+                    if (result.length > 0) {
+                        // Construimos un mensaje con los resultados
+                        const mensajes = result.map(libro => ({
+                            body: `" ${libro.titulo}, de ${libro.autor}, Valor: $${parseInt(libro.precio)} "`
+                        }));
+                        await flowDynamic([{
+                            body: `Si poseemos el libro: \n${mensajes.map(mensaje => mensaje.body).join(', \n ')}\nen nuestro inventario.
+                            \n Escribe "*volver*" para regresar al menu principal`
+                        }]);
+                    } else {
+                        await flowDynamic([{
+                            body: `Lo siento, no pudimos encontrar el libro ${ctx.body} que estás buscando en nuestro inventario.
+                            \n Escribe "*volver*" para regresar al menu principal`
+                        }]);
+                    }
+                } catch (error) {
+                    console.error('Error consultando la base de datos', error);
+                    await flowDynamic([{
+                        body: `Hubo un error al buscar el libro. Por favor, intenta de nuevo más tarde.
+                        \n Escribe "*volver*" para regresar al menu principal`
+                    }]);
+                }
             }
-        })
+        });
 
 const flowInfo = addKeyword('horario')
     .addAnswer(
@@ -102,34 +65,54 @@ const flowInfo = addKeyword('horario')
         ], null, null
     )
 
+const flowAllBooks = addKeyword('todos')
+    .addAnswer(['Recuperando todos los libros de la base de datos...'], null, async (ctx, { flowDynamic }) => {
+        try {
+            const result = await dbAdapter.getAllBooks(); // Asegúrate de tener esta función en tu adaptador
+            if (result && result.length > 0) {
+                const mensajes = result.map(libro => (`"${libro.titulo}, de ${libro.autor}, Valor: $${parseInt(libro.precio)}"`));
+                await flowDynamic([{
+                    body: `Aquí tienes todos los libros en nuestro inventario: \n${mensajes.join(', \n')} \nEscribe "*volver*" para regresar al menu principal`
+                }]);
+            } else {
+                await flowDynamic([{
+                    body: `Lo siento, no hay libros en el inventario en este momento. \nEscribe "*volver*" para regresar al menu principal`
+                }]);
+            }
+        } catch (error) {
+            console.error('Error consultando la base de datos', error);
+            await flowDynamic([{
+                body: `Hubo un error al recuperar los libros. Por favor, intenta de nuevo más tarde. \nEscribe "*volver*" para regresar al menu principal`
+            }]);
+        }
+    });
+
+
 // El flujo principal va en la ultima posicion de todos los flujos
 const flowPrincipal = addKeyword(palabrasClaveBienvenida)
     .addAnswer(
         [
-            'Bienvenido al bot de pruebas del pipa \n', 'Ingresa la palabra clave de lo que deseas',
-            '👉 "*stock*" = para buscar Titulo en nuestro inventario 📕',
-            '👉 "*horario*" =  para ver horario de atencion 🕑'
+            'Bienvenido al bot de pruebas del pipa \n',
+            'Ingresa la palabra clave de lo que deseas',
+            '👉 " *todos* " = para ver todos los libros disponibles 📚',
+            '👉 " *buscar* " = para buscar Titulo o Autor del libro en nuestro inventario 📕',
+            '👉 " *horario* " =  para ver horario de atencion 🕑',
+
         ],
         null,
         null,
-        [flowSearch, flowInfo]
+        [flowSearch, flowInfo, flowAllBooks]
     )
 
 const main = async () => {
-    const adapterDB = new PostgreSQLAdapter({
-        host: process.env.POSTGRES_DB_HOST,
-        user: process.env.POSTGRES_DB_USER,
-        database: process.env.POSTGRES_DB_NAME,
-        password: process.env.POSTGRES_DB_PASSWORD,
-        port: process.env.POSTGRES_DB_PORT,
-    })
+
     // const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowPrincipal])
+    const adapterFlow = createFlow([flowPrincipal, flowSearch, flowInfo, flowAllBooks])
     const adapterProvider = createProvider(BaileysProvider)
     createBot({
         flow: adapterFlow,
         provider: adapterProvider,
-        database: adapterDB,
+        database: dbAdapter,
     })
     QRPortalWeb()
 }
---
+++ postgres.adapter.js
@@ -0,0 +1,238 @@
+require('dotenv').config()
+'use strict';
+
+const { Pool } = require('pg');
+
+class CustomPostgresAdapter {
+  db
+  listHistory = []
+  credentials = { host: 'localhost', user: '', database: '', password: null, port: 5432 }
+
+  constructor(_credentials) {
+    this.credentials = _credentials;
+    this.init()
+  }
+
+  init = async () => {
+    const pool = new Pool(this.credentials);
+    try {
+      this.db = await pool.connect();
+      console.log('🆗 Conexión Correcta DB');
+      this.checkTableExistsAndSP();
+      return true
+    } catch (e) {
+      console.error('Error al conectar con la base de datos:', e);
+      // Cerrar el pool si la conexión falla para liberar recursos.
+      pool.end().then(() => console.log('Pool ha terminado debido a un error de conexión.'));
+      return
+    }
+  }
+
+  getPrevByNumber = async (from) => {
+    const query = `SELECT * FROM public.history WHERE phone = $1 ORDER BY created_at DESC LIMIT 1`;
+    try {
+      const result = await this.db.query(query, [from]);
+      const row = result.rows[0];
+
+      if (row) {
+        row['refSerialize'] = row.refserialize;
+        delete row.refserialize;
+      }
+
+      return row
+    } catch (error) {
+      console.error('Error al obtener la entrada anterior por número:', error);
+      throw error
+    }
+  }
+
+  save = async (ctx) => {
+    const values = [ctx.ref, ctx.keyword, ctx.answer, ctx.refSerialize, ctx.from, JSON.stringify(ctx.options)];
+    const query = `SELECT save_or_update_history_and_contact($1, $2, $3, $4, $5, $6)`;
+
+    try {
+      await this.db.query(query, values);
+      console.log('🆗 Historico creado con exito');
+    } catch (error) {
+      console.error('Error al registrar la entrada del historial:', error);
+    }
+    this.listHistory.push(ctx);
+  }
+
+  getContact = async (ctx) => {
+    const from = ctx.from;
+    const query = `SELECT * FROM public.contact WHERE phone = $1 LIMIT 1`;
+    try {
+      const result = await this.db.query(query, [from]);
+      return result.rows[0]
+    } catch (error) {
+      console.error('Error al obtener contacto por número:', error);
+      throw error
+    }
+  }
+
+  saveContact = async (ctx) => {
+    // action: u (Actualiza el valor de ctx.values), a (Agrega). Agrega por defecto.
+    const _contact = await this.getContact(ctx);
+    let jsValues = {};
+
+    if ((ctx?.action ?? 'a') === 'a') {
+      jsValues = { ..._contact.values, ...(ctx?.values ?? {}) };
+    } else {
+      jsValues = ctx?.values ?? {};
+    }
+
+    const values = [ctx.from, JSON.stringify(jsValues)];
+    const query = `SELECT save_or_update_contact($1, $2)`;
+
+    try {
+      await this.db.query(query, values);
+      console.log('🆗 Contacto guardado o actualizado con éxito');
+    } catch (error) {
+      console.error('🚫 Error al guardar o actualizar contacto:', error);
+      throw error
+    }
+  }
+
+  checkTableExistsAndSP = async () => {
+    const contact = `
+        CREATE TABLE IF NOT EXISTS contact (
+            id SERIAL PRIMARY KEY,
+            phone VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP DEFAULT current_timestamp,
+            updated_in TIMESTAMP,
+            last_interaction TIMESTAMP,
+            values JSONB
+        )`;
+
+    try {
+      await this.db.query(contact);
+      console.log('🆗 Tabla contact existe o fue creada con éxito');
+    } catch (error) {
+      console.error('🚫 Error al crear la tabla contact:', error);
+    }
+
+    const history = `
+        CREATE TABLE IF NOT EXISTS history (
+            id SERIAL PRIMARY KEY,
+            ref VARCHAR(255) NOT NULL,
+            keyword VARCHAR(255),
+            answer TEXT NOT NULL,
+            refSerialize TEXT NOT NULL,
+            phone VARCHAR(255) NOT NULL,
+            options JSONB,
+            created_at TIMESTAMP DEFAULT current_timestamp,
+            updated_in TIMESTAMP,
+            contact_id INTEGER REFERENCES contact(id)
+        )`;
+    try {
+      await this.db.query(history);
+      console.log('🆗 Tabla history existe o fue creada con éxito');
+    } catch (error) {
+      console.error('🚫 Error al crear la tabla de history:', error);
+    }
+
+    await this.createSP();
+  }
+
+  createSP = async () => {
+    const sp_suc = `
+    CREATE OR REPLACE FUNCTION save_or_update_contact(
+        in_phone VARCHAR(255),
+        in_values JSONB
+    )
+    RETURNS VOID AS
+    $$
+    DECLARE
+        contact_cursor refcursor := 'cur_contact';
+        contact_id INT;
+    BEGIN
+        SELECT id INTO contact_id FROM contact WHERE phone = in_phone;
+    
+        IF contact_id IS NULL THEN
+            INSERT INTO contact (phone, "values")
+            VALUES (in_phone, in_values);
+        ELSE
+            UPDATE contact SET "values" = in_values, updated_in = current_timestamp
+            WHERE id = contact_id;
+        END IF;
+    END;
+    $$ LANGUAGE plpgsql;`;
+
+    try {
+      await this.db.query(sp_suc);
+      console.log('🆗 Procedimiento almacenado de contacto existe o fue creada con éxito');
+    } catch (error) {
+      console.error('🚫 Error al crear el procedimiento almacenado de contacto:', error);
+    }
+
+    const sp_suhc = `
+    CREATE OR REPLACE FUNCTION save_or_update_history_and_contact(
+        in_ref VARCHAR(255),
+        in_keyword VARCHAR(255),
+        in_answer TEXT,
+        in_refserialize TEXT,
+        in_phone VARCHAR(255),
+        in_options JSONB
+    )
+    RETURNS VOID AS
+    $$
+    DECLARE
+        _contact_id INT;
+    BEGIN
+        SELECT id INTO _contact_id FROM contact WHERE phone = in_phone;
+    
+        IF _contact_id IS NULL THEN
+            INSERT INTO contact (phone)
+            VALUES (in_phone)
+            RETURNING id INTO _contact_id;
+        ELSE
+            UPDATE contact SET last_interaction = current_timestamp WHERE id = _contact_id;
+        END IF;
+    
+        INSERT INTO history (ref, keyword, answer, refserialize, phone, options, contact_id, created_at)
+        VALUES (in_ref, in_keyword, in_answer, in_refserialize, in_phone, in_options, _contact_id, current_timestamp);
+    
+    END;
+    $$ LANGUAGE plpgsql;`;
+
+    try {
+      await this.db.query(sp_suhc);
+      console.log('🆗 Procedimiento almacenado de historico existe o fue creada con éxito');
+    } catch (error) {
+      console.error('🚫 Error al crear el procedimiento almacenado de historico:', error);
+    }
+  }
+
+  searchBookByTitle = async (textSearch) => {
+    if (!this.db) {
+      console.error('La conexión a la base de datos no está definida. No se puede realizar la consulta.');
+      throw new Error('La conexión a la base de datos no está definida.');
+    }
+
+    const query = `SELECT * FROM public.libros WHERE titulo ILIKE $1 OR autor ILIKE $1`;
+    const values = [`%${textSearch}%`];
+
+    try {
+      const result = await this.db.query(query, values);
+      console.log('result.rows', result.rows);
+      return result.rows;
+    } catch (error) {
+      console.error('Error al buscar el libro por título:', error);
+      throw error;
+    }
+  }
+
+  async getAllBooks() {
+    const query = 'SELECT * FROM libros';
+    try {
+      const result = await this.db.query(query);
+      return result.rows;
+    } catch (error) {
+      console.error('Error recuperando todos los libros', error);
+      throw error;
+    }
+  }
+}
+
+module.exports = CustomPostgresAdapter;
